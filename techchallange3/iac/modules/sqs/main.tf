1fd2de76c0e54cbd56210a02e10141258c673c7a
luiz cibulski <luizcgd19@gmail.com>
2026-09-05 16:03:25 -0300

    check terrafom

--- techchallange3/iac/modules/sqs/main.tf
+++ techchallange3/iac/modules/sqs/main.tf
@@ -6,7 +6,7 @@
 resource "aws_sqs_queue" "dlq" {
   name                      = "${var.queue_name}-dlq"
   message_retention_seconds = 1209600 # 14 dias, teto do SQS
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled   = true
 
   tags = merge(var.tags, {
     Name = "${var.queue_name}-dlq"
@@ -17,8 +17,8 @@ resource "aws_sqs_queue" "this" {
   name                       = var.queue_name
   visibility_timeout_seconds = var.visibility_timeout_seconds
   message_retention_seconds  = var.message_retention_seconds
-  sqs_managed_sse_enabled = true
-  
+  sqs_managed_sse_enabled    = true
+
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq.arn
     maxReceiveCount     = var.max_receive_count
